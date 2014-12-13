543ae6602df148f6f7ae0bbd4a821b353bbfb8a2
feilengcui008 <feilengcui008@gmail.com>
2014-12-13 21:44:33 +0800

    update all

--- hualvtu_spider/middleware.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import base64
-class ProxyMiddleware(object):
-    def process_request(self,request,spider):
-        request.meta['proxy'] = YOUR_PROXY_IP
-        user_pass = "USERNAME:PASSWORD"
-        encoded = base64.encodestring(user_pass)
-        request.headers['Proxy-Authorization'] = 'Basic '+encoded
