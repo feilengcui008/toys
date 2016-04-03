d0aa75b826bca55d318c03c8d297db0ae1d0d6ca
feilengcui008 <feilengcui008@gmail.com>
2016-04-03 15:49:54 +0800

    add api testing scripts

---
+++ api_testing_scripts/README.md
@@ -0,0 +1,15 @@
+a simple script to test backend API for get and post, a little change can made to support put, delete etc..., usually testing for a Web backend API can be a lot of work, actually we can store the testcases in json file to automate and reduce the work.
+
+parameter required by the josn testcase files 
+```
+{
+  "url" : "http://test.com/"
+  "method" : "get"
+  "data" : {
+    "key1" : "value1",
+    "key2" : "value2"
+  }
+}
+```
+
+usage: python test.py test1.json
---
+++ api_testing_scripts/test.py
@@ -0,0 +1,76 @@
+#encoding: utf-8
+
+import requests 
+import sys
+import json
+
+USERNAME = "your username"
+PASSWORD = "your password"
+
+class TestScript(object):
+    url = None
+    data = None
+    method = None
+    headers = None 
+    res = None 
+    token = None 
+    cookies = None 
+    def __init__(self, url, headers = None, data = None, cookies = None, method = 'get'):
+        self.url = url
+        self.headers = headers 
+        self.data = data 
+        self.method = method 
+        # self.cookies = cookies 
+
+    def login(self):
+        username = USERNAME 
+        password = PASSWORD 
+        payload = {}
+        payload["account"] = username 
+        payload["password"] = password 
+        url = "http://x.x.x.x/login"
+        res = requests.post(url, data = payload)
+        if res.text:
+            # self.cookies = res.cookies 
+            self.token = json.loads(res.text)["msg_data"]["token"]
+        else:
+            raise Exception("login failed")
+
+    def sendRequest(self):
+        self.data["token"] = self.token
+        if self.method == 'get':
+            self.res = requests.get(url, params = self.data, headers = self.headers, cookies = self.cookies) 
+            #print self.res.content 
+            outfile = sys.stdout
+            with outfile:
+        		json.dump(json.loads(self.res.content), outfile, sort_keys=True,
+                  	indent=4, separators=(',', ': '))
+        		outfile.write('\n')
+            return 
+        if self.method == 'post':
+            self.res = requests.post(url, data= self.data, headers = self.headers, cookies = self.cookies)   
+            #print self.res.contentoutfile = sys.stdout
+            outfile = sys.stdout
+            with outfile:
+        		json.dump(json.loads(self.res.content), outfile, sort_keys=True,
+                  	indent=4, separators=(',', ': '))
+        		outfile.write('\n')
+            return 
+        
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "python test.py request_data_json_file_path"
+        sys.exit()
+    filename = sys.argv[1]
+    data = None 
+    with open(filename, 'r') as fp:
+        data = json.loads(fp.read())
+    url = data["url"]
+    method = data["method"]
+    data = data["data"]
+    headers = {}
+    ts = TestScript(url, headers, data, method = method)
+    ts.login()
+    #sys.exit()
+    ts.sendRequest()
