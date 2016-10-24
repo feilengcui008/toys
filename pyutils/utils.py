9184935c6053b8073eedde022f6701f7561fad25
feilengcui008 <feilengcui008@gmail.com>
2016-10-24 11:32:44 +0800

    add color printer for python

---
+++ pyutils/utils.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+#-*- encoding:utf-8 -*-
+
+""" some utilities """
+
+class Printer(object):
+    """ some customized terminal output methods """
+
+    RED_COLOR = '\033[1;31m'
+    GREEN_COLOR = '\033[1;32m'
+    YELLOW_COLOR = '\033[1;33m'
+    BLUE_COLOR = '\033[1;34m'
+    END_COLOR = '\033[0m'
+    BOLD_COLOR = '\033[1m'
+    UNDERLINE_COLOR = '\033[4m'
+
+    @staticmethod
+    def red(msg):
+        print "%s%s%s" %(Printer.RED_COLOR, str(msg), Printer.END_COLOR)
+
+    @staticmethod
+    def green(msg):
+        print "%s%s%s" %(Printer.GREEN_COLOR, str(msg), Printer.END_COLOR)
+
+    @staticmethod
+    def yellow(msg):
+        print "%s%s%s" %(Printer.YELLOW_COLOR, str(msg), Printer.END_COLOR)
+
+    @staticmethod
+    def blue(msg):
+        print "%s%s%s" %(Printer.BLUE_COLOR, str(msg), Printer.END_COLOR)
+
+    @staticmethod
+    def bold(msg):
+        print "%s%s%s" %(Printer.BLUE_COLOR, str(msg), Printer.END_COLOR)
