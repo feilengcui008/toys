3037892dfda74eb9ed0426cacd23531a3ddf0dc1
feilengcui008 <feilengcui008@gmail.com>
2017-06-08 09:08:27 +0800

    format code

--- commander/command.py
+++ commander/command.py
@@ -3,6 +3,7 @@
 
 import argparse
 
+
 class CommandException(Exception):
     pass
 
@@ -14,32 +15,27 @@ class Command(object):
     parser = None
     subparsers = None
     parent = None
+
     def __init__(self, name=None, run=None):
-        self.name = name 
+        self.name = name
         self.run = run
         self.parser = argparse.ArgumentParser()
         self.parser._cmd = self
-    
+
     def Execute(self):
         # goto root cmd
         if self.parent:
             return self.parent.Execute()
-        # find the command 
+        # find the command
         args = self.parser.parse_args()
         parserName = args.__dict__["_lastMatchedParser"]
-        cmd = None
-        if parserName != self.name:
+        cmd = self
+        if parserName != cmd.name:
             cmd = self._getParser(parserName)
-            if cmd == None:
-                raise CommandException("not found match command")
-        else:
-            cmd = self
         if cmd.run:
             cmd.run(cmd, args)
-    
+
     def _getParser(self, name):
-        if self.subparsers is None:
-            return None
         for k, v in self.subparsers._name_parser_map.iteritems():
             if k == name:
                 return v._cmd
@@ -53,18 +49,21 @@ class Command(object):
             raise CommandException("need Command type when AddCommand")
         for command in self.commands:
             if command.name == cmd.name:
-                raise CommandException("subcommand with the same name already exists")
+                raise CommandException(
+                    "subcommand with the same name already exists")
         if not self.subparsers:
-            self.subparsers = self.parser.add_subparsers(dest="_lastMatchedParser")
+            self.subparsers = self.parser.add_subparsers(
+                dest="_lastMatchedParser")
         cmdName = name if name is not None else cmd.name
         if not cmdName or not isinstance(cmdName, str) or cmdName == "":
             raise CommandException("subcommand name not valid")
-        if self.subparsers._name_parser_map.has_key(cmdName):
-            raise CommandException("subcommand of name %s already exists" % (cmdName,))
-        self.subparsers._name_parser_map[cmdName] = cmd.parser 
+        if cmdName in self.subparsers._name_parser_map.keys():
+            raise CommandException(
+                "subcommand of name %s already exists" % (cmdName,))
+        self.subparsers._name_parser_map[cmdName] = cmd.parser
         self.commands.append(cmd)
         cmd.parent = self
-    
+
     @property
     def Parser(self):
         return self.parser
--- commander/test.py
+++ commander/test.py
@@ -3,21 +3,27 @@
 
 from command import Command
 
+
 def runCmd(cmd, args):
     print cmd.name
     print args
 
+
 rootCmd = Command(name="root")
 
 serverCmd = Command("server", runCmd)
-serverCmd.Parser.add_argument("-p", "--port", action='store', default="8080", help='port to listen')
+serverCmd.Parser.add_argument(
+    "-p", "--port", action='store', default="8080", help='port to listen')
+serverCmd.Parser.add_argument(
+    "--finalParser", action='store', default="8080", help='port to listen')
 
 clientCmd = Command("client", runCmd)
-clientCmd.Parser.add_argument("-s", "--server", action="store", default=":8080",\
-        help='server to connect')
+clientCmd.Parser.add_argument(
+    "-s", "--server", action="store", default=":8080", help='server to conn')
 
 subSubCmd = Command("subsub", runCmd)
-subSubCmd.Parser.add_argument("-v", "--verbose", action="store_true", help="verbose mode")
+subSubCmd.Parser.add_argument(
+    "-v", "--verbose", action="store_true", help="verbose mode")
 
 
 rootCmd.AddCommand(serverCmd)
@@ -25,4 +31,3 @@ rootCmd.AddCommand(clientCmd)
 serverCmd.AddCommand(subSubCmd)
 
 subSubCmd.Execute()
-
