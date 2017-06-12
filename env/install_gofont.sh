af321bcae9d1b73522510e0e0a783a4472d4ef77
feilengcui008 <feilengcui008@gmail.com>
2017-06-12 11:11:58 +0800

    move env to dotfiles

--- env/install_gofont.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-CUR=$(cd `dirname $0`; pwd)
-cd /tmp && git clone https://github.com/golang/image
-sudo cp -r image/font/gofont/ttfs /usr/share/fonts/gofont
-rm -rf image
-cd $CUR
-
-sudo mkfontscale && sudo mkfontdir && sudo fc-cache
