368a53c66b88f5422ba7b3f2774ba56d941b57a7
feilengcui008 <feilengcui008@gmail.com>
2017-11-17 15:42:28 +0800

    update

--- simpost-theme/static/js/head.js
+++ simpost-theme/static/js/head.js
@@ -1,16 +1,15 @@
 function mouseOver() {
-  document.getElementsByClassName('sidebar-toggle-1')[0].style.display =
-      "block";
-  document.getElementsByClassName('sidebar-toggle-2')[0].style.display =
-      "block";
-  document.getElementsByClassName('sidebar-toggle-3')[0].style.display =
-      "block";
-  document.getElementsByClassName('sidebar-toggle-4')[0].style.display =
-      "block";
+    document.getElementsByClassName('sidebar-toggle-parent')[0].style.display = "block";
+    document.getElementsByClassName('sidebar-toggle-1')[0].style.display = "block";
+    document.getElementsByClassName('sidebar-toggle-2')[0].style.display = "block";
+    document.getElementsByClassName('sidebar-toggle-3')[0].style.display = "block";
+    document.getElementsByClassName('sidebar-toggle-4')[0].style.display = "block";
 }
+
 function mouseOut() {
-  document.getElementsByClassName('sidebar-toggle-1')[0].style.display = "none";
-  document.getElementsByClassName('sidebar-toggle-2')[0].style.display = "none";
-  document.getElementsByClassName('sidebar-toggle-3')[0].style.display = "none";
-  document.getElementsByClassName('sidebar-toggle-4')[0].style.display = "none";
+    document.getElementsByClassName('sidebar-toggle-1')[0].style.display = "none";
+    document.getElementsByClassName('sidebar-toggle-2')[0].style.display = "none";
+    document.getElementsByClassName('sidebar-toggle-3')[0].style.display = "none";
+    document.getElementsByClassName('sidebar-toggle-4')[0].style.display = "none";
+    document.getElementsByClassName('sidebar-toggle-parent')[0].style.display = "none";
 }
